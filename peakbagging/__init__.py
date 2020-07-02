6617ac575895e8b8d96ec68a98f68e16db947db4
Yaguang Li <yaguang.li@mail.bnu.edu.cn>
2020-07-02 23:00:02 +0800

    Removed peakbagging. Redirect it to another rep.

--- peakbagging/__init__.py
+++
@@ -1,7 +0,0 @@
-from .autoradialrgfit import *
-from .manualrgfit import *
-from .rgslide import *
-
-__all__ = autoradialrgfit.__all__
-__all__.extend(manualrgfit.__all__)
-__all__.extend(rgslide.__all__)
